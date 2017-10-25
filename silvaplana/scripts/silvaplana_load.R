129a4d73692745aef550a4c9cc7f24d9eda24511
richardjtelford <richard.telford@bio.uib.no>
2017-10-26 00:40:08 +0200

    directory change - need better solution

--- silvaplana/scripts/silvaplana_load.R
+++ silvaplana/scripts/silvaplana_load.R
@@ -6,7 +6,8 @@ recon_holocene <- read_table("data/silvaplana2009.txt", skip = 91, n_max = 134) 
 
 ## ---- read_silvaplana_Holocene_fossil
 #Load fossil data from Holocene 2009
-f <- "data/silvaplana2009.txt"
+
+f <- "silvaplana/data/silvaplana2009.txt"
 
 f2 <- readLines(f)
 YearAD <- grep("^YearAD", f2)
@@ -14,7 +15,7 @@ YearAD <- grep("^YearAD", f2)
 #fix spaces in some taxon names
 f2[YearAD[4]] <- c("YearAD   Paramect   Paraph   Psec_b   Psec_c  Pseodok    Pseuo  Pseudos    Rheocr  Smitti   Sympos    Synor  Thienne   Trisso  Zalu_zal Zalu_lin  Pentan  Tanypus  Proclad   Pottha   Undiff   Unknown")
 
-fos_holocene <- map(YearAD , function(skip) {
+fos_holocene <- purrr::map(YearAD, function(skip) {
   d <- read_table2(file = f, col_names = FALSE, skip  = skip, n_max = 134)
   h <- read.table(text = f2[skip], stringsAsFactors = FALSE, comment.char = "")
   h <- gsub("#", "_", h)
@@ -26,6 +27,8 @@ fos_holocene <- do.call(bind_cols, fos_holocene)
 # fos_holocene %>% 
 #   select(starts_with("YearAD")) %>%
 #   bind_cols(recon_holocene) %>% pn # all identical
+
+holocene_year <- fos_holocene %>% select(YearAD) 
 
 fos_holocene <- fos_holocene %>% select(-matches("^YearAD"))
 
