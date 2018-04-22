8c0b8945fe8c78bd4bd003ebda99046a88835b60
richardjtelford <richard.telford@bio.uib.no>
2018-04-22 16:48:37 +0200

    various

--- seebergsee/seeberg_climate.R
+++ seebergsee/seeberg_climate.R
@@ -1,13 +1,13 @@
 ## ---- seebergsee_climate
-seeberg_climate <- read_table("seebergsee/homog_mo_CHD.txt", skip  = 27)
-seeberg_normal <- seeberg_climate %>%  
-  filter(between(Year, 1961, 1990), Month == 7) %>% 
-  summarise(normal = mean(Temperature)) %>% pull(normal)
+seeberg_climate0 <- read_table("seebergsee/homog_mo_CHD.txt", skip  = 27)
+# seeberg_normal <- seeberg_climate %>%  
+  # filter(between(Year, 1961, 1990), Month == 7) %>% 
+  # summarise(normal = mean(Temperature)) %>% pull(normal)
 
-seeberg_climate <- seeberg_climate %>% 
+seeberg_climate <- seeberg_climate0 %>% 
   filter(Month == 7, between(Year, 1901, 2005)) %>% 
   mutate(
-    normal = seeberg_normal,
+    normal = mean(Temperature),
     anomaly = Temperature - normal
     ) 
 
@@ -17,6 +17,56 @@ seeberg_climate %>%
   geom_line() +
   geom_smooth(se = FALSE, span = 0.2) +
   scale_x_continuous(expand = c(0.01, 0))
+
+
+## ---- all_months
+seeberg_climate_12 <- seeberg_climate0 %>% 
+  group_by(Month) %>% 
+  filter(between(Year, 1901, 2005)) %>% 
+  mutate(
+    normal = mean(Temperature),
+    anomaly = Temperature - normal
+  ) 
+
+seeberg_climate_12 %>% 
+  ggplot(aes(x = Year, y = anomaly)) + 
+  geom_point() + 
+  geom_line() +
+  geom_line(aes(x = year, y = july), data = sbs_digitised, colour = "red") +
+  geom_smooth(se = FALSE, span = 0.2) +
+  scale_x_continuous(expand = c(0.01, 0)) +
+  facet_wrap(~Month)
+
+seeberg_climate0 %>% 
+  group_by(Year) %>%
+  summarise(Temperature = mean(Temperature)) %>% 
+  filter(between(Year, 1901, 2005)) %>% 
+  mutate(
+    normal = mean(Temperature),
+    anomaly = Temperature - normal
+  ) %>% 
+  ggplot(aes(x = Year, y = anomaly)) + 
+  geom_point() + 
+  geom_line() +
+  geom_line(aes(x = year, y = july), data = sbs_digitised, colour = "red") +
+  geom_smooth(se = FALSE, span = 0.2) +
+  scale_x_continuous(expand = c(0.01, 0)) 
+
+seeberg_climate0 %>% 
+  filter(between(Month, 5, 9), between(Year, 1901, 2005)) %>% 
+  group_by(Year) %>%
+  summarise(Temperature = mean(Temperature)) %>% 
+  mutate(
+    normal = mean(Temperature),
+    anomaly = Temperature - normal
+  ) %>% 
+  ggplot(aes(x = Year, y = anomaly)) + 
+  geom_point() + 
+  geom_line() +
+  geom_line(aes(x = year, y = july), data = sbs_digitised, colour = "red") +
+  geom_smooth(se = FALSE, span = 0.2) +
+  scale_x_continuous(expand = c(0.01, 0)) 
+
 
 ## ---- overplot
 makefig<-function(g){
@@ -29,6 +79,14 @@ library(magick)
 library(grid)
 see_fig6 <- image_read("seebergsee/seebergsee_fig_6.png")
 
+seeberg_climate_smo <- map_df(c(3, 8), function(w)
+  seeberg_climate %>% 
+  select(Year, anomaly) %>% 
+  zoo::rollapply(width  = w, FUN = mean) %>% 
+    as_tibble() %>% 
+  mutate(width = w)
+  ) %>% 
+  mutate(width = as.factor(width))
 
 see_clim_plot <- seeberg_climate %>% 
   ggplot(aes(x = Year, y = anomaly)) + 
@@ -36,14 +94,78 @@ see_clim_plot <- seeberg_climate %>%
     rasterGrob(as.raster(see_fig6), width=unit(1,"npc"), height=unit(1,"npc")),
     xmin = 1889, xmax = 2006, ymin = -5.1, ymax = 3.2) +
   geom_point(size = 2, shape = 1) +
-  geom_smooth(span = 0.1, se = FALSE) +
+  geom_line(aes(colour = width), seeberg_climate_smo, show.legend = FALSE) +
   scale_x_continuous(breaks = seq(1900, 2000, 20)) +
   scale_y_continuous(breaks = -4:4) +
   theme_classic() +
-  theme(plot.margin = margin(t = 10, r = 10, b = 10, l = 10))
+  theme(plot.margin = margin(t = 10, r = 10, b = 10, l = 10)) +
+  labs(y = "Anomaly °C")
 
 makefig(see_clim_plot)
 
+
+## ---- calib_in_space
+see_fig5 <- image_read("seebergsee/1-s2.0-S0277379111001090-gr5.jpg") %>% 
+  image_crop(geometry = "384x262+0+0")
+
+see_clim_plot5 <- seeberg_climate %>% 
+  ggplot(aes(x = Year, y = anomaly)) + 
+  annotation_custom(
+    rasterGrob(as.raster(see_fig5), width=unit(1,"npc"), height=unit(1,"npc")),
+    xmin = 1889, xmax = 2006, ymin = -6.9, ymax = 5.1) +
+  geom_point(size = 2, shape = 1) +
+  geom_line(aes(colour = width), seeberg_climate_smo, show.legend = FALSE) +
+  scale_x_continuous(breaks = seq(1900, 2000, 20)) +
+  scale_y_continuous(breaks = -4:4) +
+  theme_classic() +
+  theme(plot.margin = margin(t = 10, r = 10, b = 60, l = 10)) +
+  labs(y = "Anomaly °C")
+
+makefig(see_clim_plot5)
+
+
+
+## ---- digitise_cis ####
+sbs_digitised <- read.table("seebergsee/seebergsee_climate") %>% 
+  rename(year = V1, july = V2) %>% 
+  slice(-40)#bad click
+cis <- c(.7, 2, .7, 1.8, .2, -1.2, -.3, -.3, 1.7, -.1, .6, -4, 2.1, 1.1, 1.3, 1.2, 1.2, -1, 0, 0, 1.5, .9, 1.3, 1.6, 1.6)
+
+sbs_digitised <- sbs_digitised %>% 
+  mutate(cis = c(cis, rep(NA, nrow(.) -length(cis))))
+
+see_clim_plot5 <- sbs_digitised %>% 
+  ggplot(aes(x = year, y = july)) + 
+  annotation_custom(
+    rasterGrob(as.raster(see_fig5), width=unit(1,"npc"), height=unit(1,"npc")),
+    xmin = 1889, xmax = 2006, ymin = -6.9, ymax = 5.1) +
+  geom_point(size = 2, shape = 1) +
+  geom_line() +
+  geom_line(aes(y = cis), col = 2) +
+  geom_point(aes(y = cis), col = 2, size = 0.5) +
+  scale_x_continuous(breaks = seq(1900, 2000, 20)) +
+  scale_y_continuous(breaks = -4:4) +
+  theme_classic() +
+  theme(plot.margin = margin(t = 10, r = 10, b = 60, l = 10)) +
+  labs(y = "Anomaly °C") +
+  geom_hline(yintercept = seq(-4,2, .5))
+
+makefig(see_clim_plot5)
+
+
+library(magrittr)
+sbs_digitised %>% filter(round(year) <= 1960) %$% cor(july, cis)
+sbs_digitised %>% filter(round(year) <= 1980) %$% cor(july, cis)
+
+## -- recon_2012 ####
+sbs_2012 <- read_table("seebergsee/seebergsee2012.txt", skip = 105) %>% select(Dates, anomaly = `11-year`)
+
+data_frame(Reconstruction  = sbs_mod$fitted.values[, 2], 
+           #  cv = sbs_mod$predicted[, 2], 
+           Instrumental =rev(sbs_digitised$july), 
+           year = rev(sbs_digitised$year)) %>% 
+ ggplot(aes(x = year, y = Reconstruction)) + geom_line() +
+  geom_line(data = filter(sbs_2012, Dates > 1880), aes(x = Dates, y = anomaly), colour = 2)
 
 ## ---- digitised_seebergsee_climate
 sbs_digitised <- read.table("seebergsee/seebergsee_climate") %>% 
