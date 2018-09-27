3441027b691bad7bb4d1d250d50d7ce6c5d2c8db
richardjtelford <richard.telford@bio.uib.no>
2018-09-27 16:03:10 +0200

    move blog posts to own directory (several won't find resourses now)

---
+++ blog_posts/effect_smoothing.R
@@ -0,0 +1,49 @@
+
+N <-seq(5, 100, 5)
+nrep <- 1000
+cors <- plyr::ldply(N, function(n){ 
+  data_frame(n = n, cor = replicate(nrep, cor(rnorm(n), rnorm(n))))
+  })
+
+s100 <- replicate(nrep, {
+  df <- data_frame(x = rnorm(110), y = rnorm(110)) %>% 
+   # zoo::rollmean(k = 3, align = "left", na.pad = TRUE) %>% 
+    zoo::rollmean(k = 3, align = "right", na.pad = TRUE)
+   df <- df[5:104, ]
+    #df <- df[5:94, ]
+  cor(df[,"x"], df[, "y"])  
+})
+
+set.seed(42)
+df <- data_frame(x = rnorm(120), y = rnorm(120)) %>% 
+  #zoo::rollmean(k = 3, align = "left", na.pad = TRUE) %>% 
+  zoo::rollmean(k = 3, align = "right", na.pad = TRUE) %>% 
+  as_data_frame() %>% 
+  slice(5:104) %>%# cor()
+  rowid_to_column("pos") 
+
+  df %>% gather(key = key, value = value, -pos) %>% 
+  ggplot(aes(x = pos, y = value, colour = key)) + geom_line()
+
+cors %>%
+  ggplot(aes(x = n, y = cor, group = n)) + 
+  geom_violin() +
+  geom_violin(data = data_frame(cor = s100, n = 110), fill = "red")
+
+
+cors %>% 
+  group_by(n) %>% 
+  summarise(sd = sd(cor)) %>% 
+  ggplot(aes(x = n, y = sd)) + 
+  geom_point() + 
+  geom_line() +
+  geom_hline(yintercept = sd(s100), colour = "red")
+
+
+cors %>% 
+  group_by(n) %>% 
+  summarise(q95 = quantile(cor, prob = 0.95)) %>% 
+  ggplot(aes(x = n, y = q95)) + 
+  geom_point() + 
+  geom_line() +
+  geom_hline(yintercept = quantile(s100, prob = 0.95), colour = "red")
---
+++ blog_posts/transformations.R
@@ -0,0 +1,46 @@
+calibration_sets <- list()
+
+
+#SWAP
+data(SWAP)
+calibration_sets$SWAP <- list(group = "diatoms", spp = SWAP$spec, env = SWAP$pH)
+
+#Larocque 2001
+
+env <- read_excel("abisko/data/SwedenEnvData.xlsx", sheet = "EnvData") %>% 
+  mutate(`Lake number` = as.numeric(`Lake number`)) %>% 
+  filter(!is.na(`Lake number`))
+
+spp <- read_excel("abisko/data/Sweden chiro.xlsx", sheet = "Sheet1") %>% 
+  verify(X__1 == paste0("T", env$`Lake number`)) %>% 
+  select(-X__1)
+
+calibration_sets$Larocque2001 <- list(group = "chironomids", spp = spp, env = env$`Tjul (°C)`)
+
+
+
+multi_mod <- function(cal, FUN, ...){
+  mod <- list()
+  with(cal, {
+    mod$raw <<- FUN(spp, env, ...) %>% crossval()
+    mod$sqrt <<- FUN(sqrt(spp), env, ...) %>% crossval()
+    mod$log <<- FUN(log1p(spp), env, ...) %>% crossval()
+    mod$log1000 <<- FUN(log1p(spp * 10), env, ...) %>% crossval()
+  })
+  result <- mod %>% map(performance) %>% map("crossval") %>% 
+    map(as.data.frame) %>% 
+    map_df(rownames_to_column, var = "setting", .id = "transform")
+  result
+}
+
+
+result <- calibration_sets %>% map_df(multi_mod, FUN = WAPLS, .id = "cali")
+
+result %>% 
+  mutate(transform = factor(transform, levels = c("raw", "sqrt", "log", "log1000")), 
+         setting = gsub("Comp", "", setting), 
+         setting = as.numeric(setting)) %>% 
+  ggplot(aes(x = setting, y = RMSE, colour = transform)) +
+  geom_line() +
+  facet_wrap(~ cali, scales = "free_y") +
+  labs(x = "Number of components", y = "RMSEP", colour = "Transformation")
