fe60c58bc65e55be00f10d0a0d4c81a8055d491c
richardjtelford <richard.telford@bio.uib.no>
2018-10-30 22:30:25 +0100

    checking climate series

--- scripts/speke/speke_original.R
+++ scripts/speke/speke_original.R
@@ -93,3 +93,87 @@ est_n %>%
 
 percent_checker(speke_fos, digits = 2) %>% 
   select(-one_max, -one_min, -est_max, -est_min)
+
+
+valley2 <- valley %>% 
+  select(year,Jul) %>% 
+  mutate(
+    smo3 = rollmean(Jul, k = 3, na.pad = TRUE, align = "center"),
+    smo5 = rollmean(Jul, k = 5, na.pad = TRUE, align = "center")) %>% 
+  select(-Jul) %>% 
+  gather(key = smooth, value = Jul, -year)
+
+speke_chron %>% 
+  mutate(top = CodeNum, base = lead(CodeNum)) %>% 
+  crossing(valley2) %>% 
+  filter(year == base) %>% 
+  ggplot(aes(x = base, y = Jul, colour = smooth)) + 
+  geom_line() + geom_point() +
+  geom_line(data = speke_temp2, aes(x = year, y = inst), colour  = "blue", alpha = 0.5) + 
+  geom_point(data = speke_temp2, aes(x = year, y = inst), colour  = "blue", alpha = 0.5) +
+  geom_line(data = valley, aes(x = year, y = Jul), colour = "grey40")
+
+speke_chron %>%
+  mutate(top = CodeNum, base = lead(CodeNum)) %>%
+  crossing(valley) %>%
+  filter(year >= base, year < top) %>%
+  group_by(base) %>%
+  summarise(Jul = mean(Jul)) %>% 
+  arrange(desc(base)) %>% 
+  bind_cols(speke_pred %>% slice(1:17)) %>% 
+  slice(-17) %$% 
+  cor.test(Jul, pred) 
+  
+analogue::timetrack(speke_spp1, speke_fos1, speke_env$July) %>% plot()
+
+speke_n <- data_frame(taxa = names(speke_spp), 
+                      max = sapply(speke_spp, max), 
+                      n2 = Hill.N2(speke_spp))
+speke_fosn <- data_frame(taxa = names(speke_fos), 
+                      max = sapply(speke_fos, max), 
+                      n2 = Hill.N2(speke_fos)) %>% 
+  filter(max > 0) 
+
+speke_max_n <- speke_n %>% 
+  full_join(speke_fosn, by = "taxa", suffix = c("_spp", "_fos")) 
+
+speke_max_n %>% 
+  ggplot(aes(x = max_spp, y = max_fos, colour = n2_spp > 5)) + 
+  geom_point() +
+  geom_abline() +
+  geom_text(data = filter(speke_max_n, max_fos > 2 * max_spp), aes(label = taxa, colour = n2_spp > 5))
+
+
+speke_spp
+
+
+
+
+####
+fos <- decostand(speke_fos, method="hellinger")
+spp <- decostand(speke_spp, method="hellinger")
+env <- speke_env$July
+plot(cca(spp~env))
+
+rlen<-analogue::residLen(spp, env, fos, method="cca")
+rlen
+plot(rlen) # distribution of modern and fossil residual lengths
+plot(speke_chron$CodeNum, rlen$passive, ylab="Squared residual length", xlab="Depth")
+abline(h=quantile(rlen$train, probs = c(0.75,0.95)), col=c("orange", "red"))
+#
+
+goodpoorbad <- quantile(rlen$train, prob=c(0.75, 0.95))
+qualitybands <- data.frame(xmin = rep(-Inf, 3),
+                           xmax = rep(Inf, 3),
+                           ymax = c(goodpoorbad, Inf),
+                           ymin = c(-Inf, goodpoorbad),
+                           fill = factor(c("Good", "Fair", "None"), levels = c("None", "Fair", "Good")))
+
+fillscale <-  scale_fill_manual(values = c("salmon", "lightyellow", "skyblue"), name = "Analogue Quality")
+
+g <- ggplot(data.frame(chron = speke_chron$CodeNum, analogue =  rlen$passive)) +
+  geom_point(aes(x = chron, y = analogue)) +
+  labs(x = "Date CE", y = "Squared residual length") +
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = fill), qualitybands, alpha = .5) +
+  fillscale
+print(g)
